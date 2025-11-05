84e0aeee47cefbe9795cff08f973a917cf2af57e
Tyfee <zetsubou1403@gmail.com>
2025-11-05 16:39:14 -0300

    bigger set for unit testing

--- testing/test.cpp
+++ testing/test.cpp
@@ -3,8 +3,17 @@
 #include "../src/lang_it.h"
 
 
-TEST_CASE("PT_EN") {
-    CHECK(translate("eu como arroz", "pt", "en", 0) == "i eat rice");
-    CHECK(translate("eu não como arroz", "pt", "en", 0) == "i don't eat rice");
+TEST_CASE("Irregular verbs from portuguese to english") {
+    CHECK(translate("comeu", "pt", "en", 0) == "ate");
+    CHECK(translate("matou", "pt", "en", 0) == "killed");
+    CHECK(translate("comprou", "pt", "en", 0) == "bought");
+    CHECK(translate("alimentou", "pt", "en", 0) == "fed");
+    CHECK(translate("bebeu", "pt", "en", 0) == "drank");
+    CHECK(translate("escreveu", "pt", "en", 0) == "wrote");
  
+}
+TEST_CASE("Irregular plural from portuguese to english") {
+    CHECK(translate("dentes", "pt", "en", 0) == "teeth");
+    CHECK(translate("homens", "pt", "en", 0) == "men");
+    CHECK(translate("peixes", "pt", "en", 0) == "fish");
 }
